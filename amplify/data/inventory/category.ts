94c22346f55e375f7af758332e9fecac032ef523
AndrewVKerr2001 <148022069+AndrewVKerr2001@users.noreply.github.com>
2025-06-09 02:27:21 -0500

    Fixed incorrect hasMany relationship on category model.

--- amplify/data/inventory/category.ts
+++ amplify/data/inventory/category.ts
@@ -3,6 +3,6 @@ import { a } from "@aws-amplify/backend";
 export const Category = a
   .model({
     name: a.string().required(),
-    products: a.hasMany("Stock", "categoryId"),
+    products: a.hasMany("Product", "categoryId"),
   })
   .authorization((allow) => [allow.authenticated()]);
